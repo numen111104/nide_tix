2f7c616822d47f6dbe356511cea562d220a19079
numen111104 <numannasyarmz11@gmail.com>
2024-03-16 15:41:45 +0900

    feat: Adding bottom float action and bottom bar icons

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -17,6 +17,73 @@ class HomePage extends StatelessWidget {
             'Nide Tix',
           ),
         ),
+        bottomNavigationBar: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          notchMargin: 6,
+          clipBehavior: Clip.antiAlias,
+          elevation: 0,
+          color: whiteColor,
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: whiteColor,
+            elevation: 0,
+            selectedItemColor: blueColor,
+            unselectedItemColor: blackColor,
+            showUnselectedLabels: true,
+            showSelectedLabels: true,
+            selectedLabelStyle: blueTextStyle.copyWith(
+              fontSize: 10,
+              fontWeight: medium,
+            ),
+            unselectedLabelStyle: blackTextStyle.copyWith(
+              fontSize: 10,
+              fontWeight: medium,
+            ),
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(
+                  IonIcons.home,
+                  weight: 14,
+                ),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  IonIcons.map,
+                  weight: 14,
+                ),
+                label: 'Destinations',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  IonIcons.ticket,
+                  weight: 14,
+                ),
+                label: 'Ticket',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  IonIcons.bookmarks,
+                  weight: 14,
+                ),
+                label: 'History',
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: purpleColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50),
+          ),
+          child: Icon(
+            IonIcons.bag_add,
+            color: lightBackgroundColor,
+            weight: 50,
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         body: SafeArea(
             child: ListView(
           children: [
